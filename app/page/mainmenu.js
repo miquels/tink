2435015d6c81fb64c9dae636a1213060bd6a832c
Miquel van Smoorenburg <mike@langeraar.net>
2016-01-27 22:45:09 +0100

    mainmenu update

--- app/page/mainmenu.js
+++ app/page/mainmenu.js
@@ -17,7 +17,8 @@ var Backbone		= require('backbone'),
 	model			= require('./mainmenu-model.js'),
 	appconfig		= require('../js/appconfig.js'),
 	_				= require('underscore'),
-	$				= require('jquery');
+	$				= require('jquery'),
+	Key				= require('../js/keys.js');
 
 module.exports = Backbone.View.extend({
 
@@ -25,6 +26,7 @@ module.exports = Backbone.View.extend({
 
 	events: {
 			"keydown":		"_keyDown",
+			"keyup":		"_keyUp",
 	},
 
 	initialize: function(options) {
@@ -56,6 +58,9 @@ module.exports = Backbone.View.extend({
 		// initialize the subviews.
 		Page.createViews(this, { model: this.model });
 
+		this.$el.focus();
+		this.$el.find('#main-movies img').focus();
+
 		var self = this;
 		this.$el.find('.main-button').each(function() {
 			$(this).click(function(ev) {
@@ -68,6 +73,8 @@ module.exports = Backbone.View.extend({
 	show: function(options) {
 		console.log('Main.show', options);
 		this.$el.show();
+		this.$el.find('#main-movies img').focus();
+
 		// XXX hack
 		var i = this.model.get('bgimg');
 		this.model.set('bgimg', null,  { silent: true });
@@ -79,8 +86,33 @@ module.exports = Backbone.View.extend({
 		this.$el.hide();
 	},
 
+	_keyUp: function(ev) {
+		//console.log('UP key: ' + ev.which + ' mapped to ' + Key.map(ev));
+	},
+
 	_keyDown: function(ev) {
-		
+		console.log('XXX key: ' + ev.which + ' mapped to ' + Key.map(ev));
+		switch (Key.map(ev)) {
+			case Key.Enter:
+				console.log('XXX call _click');
+				this._click(ev, $(ev.target));
+				break;
+			case Key.Left:
+			case Key.Right:
+				var n = this.$el.find(':focus').attr('id') || '';
+				var img;
+				if (n.match(/movies/)) {
+					console.log('focus tvshows');
+					img = this.$el.find('#main-tvshows-img');
+				} else {
+					console.log('focus movies');
+					img = this.$el.find('#main-movies-img');
+				}
+				if (img && img.length) {
+					this.model.set('bgimg', img.attr('src'));
+					img.focus();
+				}
+		}
 	},
 
 	_click: function(ev, el) {
@@ -89,8 +121,10 @@ module.exports = Backbone.View.extend({
 		if (img.length) {
 			this.model.set('bgimg', img.attr('src'));
 		}
+		console.log('XXX _click ' + el.attr('id'));
 		switch (el.attr('id')) {
 			case 'main-movies':
+			case 'main-movies-img':
 				if (this.moviesurl) {
 					Page.switchPage('movies', {
 						url:	this.moviesurl,
@@ -98,6 +132,7 @@ module.exports = Backbone.View.extend({
 				}
 				break;
 			case 'main-tvshows':
+			case 'main-tvshows-img':
 				console.log('main-tvshows', this.tvshowsurl);
 				if (this.tvshowsurl) {
 					Page.switchPage('tvshows', {
